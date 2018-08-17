e4e8757954404d85bb31f0827c5caad0485095ec
fr-xiaoli <xiaoli.shen@fastretailing.com>
2018-08-18 00:08:45 +0900

    npm install and configure mongoURI

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  // mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://alabebop:wancaii321@ds123562.mlab.com:23562/blog_dev',
   cookieKey: '123123123'
 };
